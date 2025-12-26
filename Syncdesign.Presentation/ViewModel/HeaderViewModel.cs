2ac3a4e99798ff9e4a3357905b1c1a314293b581
CHMOSE023 <75505898@qq.com>
2025-12-26 11:08:48 +0800

    顶部按钮命令接口

--- Syncdesign.Presentation/ViewModel/HeaderViewModel.cs
+++ Syncdesign.Presentation/ViewModel/HeaderViewModel.cs
@@ -5,20 +5,23 @@ using Syncdesign.Presentation.Model;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
-
 namespace Syncdesign.Presentation.ViewModel;
 public partial class HeaderViewModel : ObservableObject
 {
     public HeaderViewModel()
     {
         UserName ="用户1";
+        Signature = "打造最好用的协同设计软件！";
     }
 
     [ObservableProperty]
     private  string? userName;
 
     [ObservableProperty]
-    private ObservableCollection<SearchItem> signatures = new();
+    private string? signature;
+
+    [ObservableProperty]
+    private ObservableCollection<SearchItem> searchItems = new();
 
     [ObservableProperty]
     private string? keyword;
@@ -28,16 +31,16 @@ public partial class HeaderViewModel : ObservableObject
     {
         Debug.WriteLine($"Search keyword: {keyword}");
 
-        Signatures.Clear();
+        SearchItems.Clear();
 
         if (string.IsNullOrWhiteSpace(keyword))
             return;
 
         // 模拟搜索结果
-        Signatures.Add(new SearchItem(keyword + "A", "A"));
-        Signatures.Add(new SearchItem(keyword + "B", "B"));
-        Signatures.Add(new SearchItem(keyword + "C", "C"));
-        Signatures.Add(new SearchItem(keyword + "D", "D"));
+        SearchItems.Add(new SearchItem(keyword + "A", "A"));
+        SearchItems.Add(new SearchItem(keyword + "B", "B"));
+        SearchItems.Add(new SearchItem(keyword + "C", "C"));
+        SearchItems.Add(new SearchItem(keyword + "D", "D"));
     }
 
     [RelayCommand]
@@ -50,6 +53,46 @@ public partial class HeaderViewModel : ObservableObject
         WeakReferenceMessenger.Default.Send(new SearchItemMessage(searchItem)); 
 
         Keyword = string.Empty;
-    } 
-   
+    }
+
+    [RelayCommand]
+    private void Add()
+    {
+        Debug.WriteLine($"Add:{DateTime.Now}");
+    }
+
+    [RelayCommand]
+    private void Archive()
+    {
+        Debug.WriteLine($"Archive:{DateTime.Now}");
+    }
+    [RelayCommand]
+    private void CloudDown()
+    {
+        Debug.WriteLine($"CloudDown:{DateTime.Now}");
+    }
+    [RelayCommand]
+    private void CloudUp()
+    {
+        Debug.WriteLine($"CloudUp:{DateTime.Now}");
+    }
+    [RelayCommand]
+    private void CloudSave()
+    {
+        Debug.WriteLine($"CloudSave:{DateTime.Now}");
+    }
+
+    [RelayCommand]
+    private void AppsAdd()
+    {
+        Debug.WriteLine($"AppsAdd:{DateTime.Now}");
+    }
+
+    /// <summary>
+    /// 个性签名属性发生变化
+    /// </summary> 
+    partial void OnSignatureChanged(string? oldValue, string? newValue)
+    {
+        Debug.WriteLine($"OnSignatureChanged: oldValue {oldValue} , newValue {newValue}");
+    }
 }
