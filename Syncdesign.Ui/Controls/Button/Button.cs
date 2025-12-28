76741545e88a29a7b7be0ed04176959b496a95ed
CHMOSE023 <75505898@qq.com>
2025-12-29 00:13:58 +0800

    联系人->顶部按钮

--- Syncdesign.Ui/Controls/Button/Button.cs
+++ Syncdesign.Ui/Controls/Button/Button.cs
@@ -1,6 +1,25 @@
-﻿namespace Syncdesign.Ui.Controls
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Syncdesign.Ui.Controls;
+
+public class Button : System.Windows.Controls.Button
 {
-    public class Button : System.Windows.Controls.Button
+    public static readonly DependencyProperty HoverBackgroundProperty =
+        DependencyProperty.Register(nameof(HoverBackground), typeof(Brush), typeof(Button), new PropertyMetadata(Brushes.LightGray));
+
+    public Brush HoverBackground
     {
+        get => (Brush)GetValue(HoverBackgroundProperty);
+        set => SetValue(HoverBackgroundProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(Button), new PropertyMetadata(new CornerRadius(6)));
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
     }
 }
